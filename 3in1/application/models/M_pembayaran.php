db2b1e1a1cf563299d3642e0a6229fa89d3a5f78
Jihad Aulia <Jihad Aulia@JihadAulia>
2018-05-10 15:34:35 +0700

    hehe

--- 3in1/application/models/M_pembayaran.php
+++
@@ -1,9 +0,0 @@
-<?php
-class M_pembayaran extends CI_Model {
-  function data_menu($meja){
-    $query = $this-> db ->query ("select distinct memesan_makanan.nama_menu, sum(jumlah) as total, harga from memesan_makanan, menu where id_pengunjung IN (select id_pengunjung from pengunjung where nomor_meja = $meja) and menu.nama_menu=memesan_makanan.nama_menu group by nama_menu");
-    return $query->result();
-  }
-}
-
-?>
