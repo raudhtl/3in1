ffd7cf035b5ad1f23ff6e83a2c6a84e680a06c52
Jihad Aulia <Jihad Aulia@JihadAulia>
2018-05-22 17:40:53 +0700

    hehe

--- application/controllers/Koki.php
+++ application/controllers/Koki.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 class Koki extends CI_Controller {
   function index(){
@@ -15,21 +14,3 @@ class Koki extends CI_Controller {
   }
 }
 ?>
-=======
-<?php
-class Koki extends CI_Controller {
-  function index(){
-
-    $this->load->helper("url");
-    $this->load->helper('form');
-
-    $this->load->database();
-    $this->load->model('M_pembayaran');
-    $this->load->model('M_pembayaran');
-    $data['bayar'] = $this->M_pembayaran->data_menu($this->session->userdata('meja_bayar'));
-    $this->load->view('v_koki', $data);
-
-  }
-}
-?>
->>>>>>> refs/remotes/origin/tambahpesanan
