a9127c95c06f33a85f73af2d458b370f05db0708
Jihad Aulia <Jihad Aulia@JihadAulia>
2018-05-22 17:09:07 +0700

    hehe

--- application/views/v_koki.php
+++ application/views/v_koki.php
@@ -176,7 +176,7 @@ input[type=checkbox]:checked + label {
 						<li><a   href="blog.html">Blog</a></li>
 						<li><a class="active" href="typo.html">Codes</a></li>
 						<li><a  href="events.html">Events</a></li>
-						<li><a  href="contact.html">Contact</a></li>
+
 					</ul>
 				</div>
 			<script>
