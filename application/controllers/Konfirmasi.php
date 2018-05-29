e587e949cefb3a043019137b5d83c86058a82b77
khairaini <Rainy@DESKTOP-9JUS5M0>
2018-05-29 16:22:56 +0700

    lalala

--- application/controllers/Konfirmasi.php
+++ application/controllers/Konfirmasi.php
@@ -1,39 +1,118 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Konfirmasi Pesanan</title>
+<link href="<?=base_url();?>assets/css/form2.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="<?=base_url();?>assets/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="<?=base_url();?>assets/js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="<?=base_url();?>assets/css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Cookery Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!---->
+<link href='//fonts.googleapis.com/css?family=Raleway:400,200,100,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
+<link href="<?=base_url();?>assets/css/styles.css" rel="stylesheet">
+<!-- animation-effect -->
+<link href="<?=base_url();?>assets/css/animate.min.css" rel="stylesheet">
+<script src="<?=base_url();?>assets/js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+<!-- //animation-effect -->
 
-class Notifikasi extends CI_Controller {
+</head>
+<body>
+<div class="header head">
+	<div class="container">
+		<div class="logo animated wow pulse" data-wow-duration="1000ms" data-wow-delay="500ms">
+			<h1><a href="index.html"><span>3IN1</span></a></h1>
+		</div>
+		<div class="nav-icon">
+			<a href="#" class="navicon"></a>
+				<div class="toggle">
+					<ul class="toggle-menu">
+						<li><a  href="index.html">Home</a></li>
+						<li><a  href="menu.html">Menu</a></li>
+						<li><a class="active"  href="blog.html">Blog</a></li>
+						<li><a  href="typo.html">Codes</a></li>
+						<li><a  href="events.html">Events</a></li>
+						<li><a  href="contact.html">Contact</a></li>
+					</ul>
+				</div>
+			<script>
+			$('.navicon').on('click', function (e) {
+			  e.preventDefault();
+			  $(this).toggleClass('navicon--active');
+			  $('.toggle').toggleClass('toggle--active');
+			});
+			</script>
+		</div>
+	<div class="clearfix"></div>
+	</div>
+	<!-- start search-->
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-    $this->load->model('M_pilihmeja');
-    $data['barang']= $this->M_pilihmeja->lihatStatus();
-		$this->load->view('v_notif',$data);
+</div>
+<!--content-->
+<div class="blog">
+			<div class="buttonshop">
+			<ul>
+        <form method="post" action="Konfirmasi/pilih">
+			<li><button type="submit" value="1" class="register registerLink" name="pilih">
+								 TAMBAH PESANAN
+						 </button></li>
+			<li><button type="submit" value="2" class="register registerLink" name="pilih">
+					     <a >PANGGIL PELAYAN</a>
+			</button></li>
+    </form>
+			</ul>
+  </div>
+</div>
+<!--//content-->
+<!--footer-->
+	<div class="footer">
+		<div class="container">
+			<div class="footer-head">
+				<div class="col-md-8 footer-top animated wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<ul class=" in">
+						<li><a href="index.html">Home</a></li>
+						<li><a  href="menu.html">Menu</a></li>
+						<li><a  href="contact.html">Contact</a></li>
+					</ul>
+						<span>There are many variations of passages</span>
+				</div>
+				<div class="col-md-4 footer-bottom  animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<h2>Follow Us</h2>
+					<label><i class="glyphicon glyphicon-menu-up"></i></label>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.</p>
+					<ul class="social-ic">
+						<li><a href="#"><i></i></a></li>
+						<li><a href="#"><i class="ic"></i></a></li>
+						<li><a href="#"><i class="ic1"></i></a></li>
+						<li><a href="#"><i class="ic2"></i></a></li>
+						<li><a href="#"><i class="ic3"></i></a></li>
+					</ul>
 
-	}
-	public function remove(){
-		$pilihan=$this->input->post('pilihan');
-		$this->session->unset_userdata('no_meja'.$pilihan);
-		$this->session->unset_userdata('koki');
-		for($i=1 ; $i<=9; $i++){
-			if($this->session->userdata('no_meja'.$i) != TRUE && $i==9){
-					$this->session->unset_userdata('notif');
-			}
-		}
-		redirect('Notifikasi');
-	}
-}
+				</div>
+			<div class="clearfix"> </div>
+
+			</div>
+			<p class="footer-class animated wow bounce" data-wow-duration="1000ms" data-wow-delay="500ms">&copy; 2016 Cookery . All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+		</div>
+	</div>
+	<!--//footer-->
+
+</body>
+</html>
