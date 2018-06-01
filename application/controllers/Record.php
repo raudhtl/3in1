f83ffbb5df2cfaec806bee07dfc409848b896073
Irham Habibi <Irham Habibi@DESKTOP-VB7AUU9>
2018-06-01 15:39:35 +0800

    dddddd

--- application/controllers/Record.php
+++ application/controllers/Record.php
@@ -20,6 +20,32 @@ class Record extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('v_record');
+		$this->load->model('M_record');
+		$data['record'] = $this->M_record->record_hari_ini();
+		$this->load->view('v_record', $data);
 	}
+	public function cari(){
+		if($this->input->post('selesai') == 0){
+			redirect('Pilih_meja');
+		}
+		$this->load->model('M_record');
+		$pilihan = $this->input->post('berdasarkan');
+		$waktu = $this->input->post('waktu');
+		if ($pilihan == 1){
+			$this->session->set_flashdata('pilihan', TRUE);
+			$nomor_meja = $this->input->post('nomor_meja');
+			$data['record'] = $this->M_record->cari_record1($waktu, $nomor_meja);
+		}
+		elseif ($pilihan == 2) {
+			$this->session->set_flashdata('pilihan', TRUE);
+			$nomor_meja = $this->input->post('nomor_meja');
+			$nama = $this->input->post('nama');
+			$data['record'] = $this->M_record->cari_record2($waktu, $nomor_meja, $nama);
+		}
+		else{
+			$data['record'] = $this->M_record->cari_record($waktu);
+		}
+		$this->load->view('v_record', $data);
+	}
+
 }
